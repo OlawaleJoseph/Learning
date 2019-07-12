f9f2bffe6916c609f474475c2a3092a7e12e40fd
Olawale <adedekojoseph@gmail.com>
2019-07-12 09:32:14 +0100

    tenth commit

--- server/models/database.js
+++ server/models/database.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/prefer-default-export */
 import dotenv from 'dotenv';
 import { Pool } from 'pg';
 
@@ -6,7 +7,11 @@ dotenv.config();
 const connectionString = process.env.NODE_ENV === 'development' ? process.env.devDb : process.env.dbUrl;
 
 export const pool = new Pool({
-  connectionString,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: 5432,
 });
 
 pool.on('connect', () => {

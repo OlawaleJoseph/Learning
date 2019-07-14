d8f09104416a6a9a26c58835d5cd5761ea0ed935
Olawale <adedekojoseph@gmail.com>
2019-07-14 19:00:29 +0100

    update index route

--- server/index.js
+++ server/index.js
@@ -13,17 +13,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
-app.use('/', (req, res) => {
-  res.send('Welcome to Wayfarer api')
+app.get('/', (req, res) => {
+  res.send('Welcome to Way Farer');
 });
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 app.use('/api/v1/auth', userRouter);
 app.use('/api/v1/buses', busRouter);
 app.use('/api/v1/trips', tripRouter);
 app.use('/api/v1/bookings', bookingRouter);
-app.get('/api/v1', (req, res) => {
-  res.send('Welcome to Way Farer');
-});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
